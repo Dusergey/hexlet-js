c11b8d115936658f8d511f5739ad8e72f5cce5b2
Sergey <sergey200312@bk.ru>
2024-09-15 19:22:21 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 export default (list) => {
     let lastarr = list[list.length - 1];
     return lastarr;
 }
+=======
+export default str => str.split('').reverse().join('');
+>>>>>>> 00825c1 (tests files)
